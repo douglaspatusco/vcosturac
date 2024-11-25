b8784b38617cc501a4904ca527603acbde12ba6d
Doug <doug.edgates@gmail.com>
2024-11-25 10:17:39 -0300

    Header now adjust with scroll

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,9 @@
 import { AppProps } from 'next/app'
 
-<<<<<<< HEAD
 import GlobalStyle from '@/styles/GlobalStyles'
 import { Content } from './styles'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
-=======
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
-
-import GlobalStyle from '@/styles/GlobalStyles';
-import { Content } from './styles';
->>>>>>> main
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
