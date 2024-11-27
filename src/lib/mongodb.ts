0d72256be7e1825aafcbf51dcd9f843d0ea94290
Doug <doug.edgates@gmail.com>
2024-11-27 15:18:04 -0300

    Testing MongoDB api

---
+++ src/lib/mongodb.ts
@@ -0,0 +1,26 @@
+import { MongoClient } from 'mongodb'
+
+const uri = process.env.MONGODB_URI || '' // Adicione sua URI no arquivo .env
+const options = {}
+
+let client: MongoClient
+let clientPromise: Promise<MongoClient>
+
+if (!process.env.MONGODB_URI) {
+  throw new Error('Por favor, adicione a variável MONGODB_URI no arquivo .env')
+}
+
+if (process.env.NODE_ENV === 'development') {
+  // Para evitar criar múltiplas conexões em desenvolvimento
+  if (!(global as any)._mongoClientPromise) {
+    client = new MongoClient(uri, options)
+    ;(global as any)._mongoClientPromise = client.connect()
+  }
+  clientPromise = (global as any)._mongoClientPromise
+} else {
+  // Em produção, sempre cria uma nova conexão
+  client = new MongoClient(uri, options)
+  clientPromise = client.connect()
+}
+
+export default clientPromise
--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,27 @@
+<<<<<<< Updated upstream
 declare Product = {
   id: number,
   thumb: string,
   name: string,
   description: string
+=======
+declare interface Image {
+  title: string
+  url: string
+}
+
+declare type Product = {
+  id: number
+  name: string
+  price: number
+  description: string
+  details: string
+  medias: {
+    thumbnail: string
+    floral?: Image[]
+    listrado?: Image[]
+    geometrico?: Image[]
+    semEstampa?: Image[]
+  }
+>>>>>>> Stashed changes
 }
