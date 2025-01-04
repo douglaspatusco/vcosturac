c626c5bdcb36bc59d930bd051f9e4c531dd49b49
Doug <doug.edgates@gmail.com>
2025-01-04 17:01:03 -0300

    refactor produtoUtils.ts

--- src/utils/produtoUtils.ts
+++ src/utils/produtoUtils.ts
@@ -3,13 +3,17 @@ import { AppDispatch } from '@/store'
 import { addItemToCart, toggleCart } from '@/store/reducers/cartSlice'
 import { PrintsImages, Product } from '@/types/product'
 
+// URL base para as imagens de estampas
+const BASE_URL =
+  'https://raw.githubusercontent.com/eyelexx/vcosturac/refs/heads/main/src/public/images/estampas/'
+
 // ---------- ZOOM NA IMAGEM
 
 // Calcula a origem da transformação para o efeito de zoom.
 export const calculateMousePosition = (
   e: React.MouseEvent<HTMLDivElement>,
   setTransformOrigin: Dispatch<SetStateAction<string>>
-) => {
+): void => {
   const rect = e.currentTarget.getBoundingClientRect()
   const x = ((e.clientX - rect.left) / rect.width) * 100
   const y = ((e.clientY - rect.top) / rect.height) * 100
@@ -17,26 +21,29 @@ export const calculateMousePosition = (
 }
 
 // Ativa o zoom na imagem.
-export const enableZoom = (setIsZoomed: Dispatch<SetStateAction<boolean>>) => {
+export const enableZoom = (
+  setIsZoomed: Dispatch<SetStateAction<boolean>>
+): void => {
   setIsZoomed(true)
 }
 
 // Desativa o zoom na imagem.
-export const disableZoom = (setIsZoomed: Dispatch<SetStateAction<boolean>>) => {
+export const disableZoom = (
+  setIsZoomed: Dispatch<SetStateAction<boolean>>
+): void => {
   setIsZoomed(false)
 }
 
 // ---------- ESTAMPAS
 
 // Obtém a URL de uma estampa específica.
-export const getPrintImageUrl = (key: string) =>
-  `https://raw.githubusercontent.com/eyelexx/vcosturac/refs/heads/main/src/public/images/estampas/${key}.jpg`
+export const getPrintImageUrl = (key: string): string => `${BASE_URL}${key}.jpg`
 
 // Seleciona a imagem principal para exibição.
 export const handleThumbnailClick = (
   image: PrintsImages,
   setMainImage: React.Dispatch<React.SetStateAction<PrintsImages | null>>
-) => {
+): void => {
   setMainImage(image)
 }
 
