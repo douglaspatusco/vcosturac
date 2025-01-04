5281120aaf9d7ab64424b7413f14fd16cbb023d1
Doug <doug.edgates@gmail.com>
2025-01-04 16:22:37 -0300

    refaactor utility.ts

--- src/services/utility.ts
+++ src/services/utility.ts
@@ -5,6 +5,12 @@ export const getFirstLetter = (word?: string): string => {
   return word.charAt(0).toUpperCase() + word.slice(1)
 }
 
-export const formattedPrice = (price) => {
-  return `R$ ${price.toLocaleString('pt-BR', { minimumFractionDigits: 2, maximumFractionDigits: 2 })}`
+export const formattedPrice = (price: number): string => {
+  if (isNaN(price)) {
+    return 'R$ 0,00'
+  }
+  return `R$ ${price.toLocaleString('pt-BR', {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  })}`
 }
